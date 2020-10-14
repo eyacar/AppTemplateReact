20fc43068c9b21831b8fd1fe3cd42c2d12520e21
eyacar <68761021+eyacar@users.noreply.github.com>
2020-10-14 13:24:50 -0300

    Updates

---
+++ precache-manifest.be0870d9122f6d2c7d23ecdeb12d0ff1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a54aa18c4c6a4eb0f2933aa535f214c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e11dac04610c7147b9c5",
+    "url": "/static/css/main.07d0665a.chunk.css"
+  },
+  {
+    "revision": "443ed0ddb259f76ea432",
+    "url": "/static/js/2.4c17e5e4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4c17e5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11dac04610c7147b9c5",
+    "url": "/static/js/main.2c56d8fd.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  },
+  {
+    "revision": "0c3e4b15c5aeb67b984e4c5996d79c50",
+    "url": "/static/media/logo.0c3e4b15.svg"
+  }
+]);
